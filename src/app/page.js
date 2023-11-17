e7f44a860527dec69d1faf77e624c46c7ccaa454
JessicaaSun <sunjessica05@gmail.com>
2023-11-17 11:14:24 +0700

    added admin page

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,18 @@
 import React from 'react'
 import UserBar from "@/components/Table/UserBar";
 import UserLine from "@/components/Table/UserLine";
+import UserTable from '@/components/Table/UserTable';
 
 export default function Homepage() {
   return (
-      <div className={"grid md:grid-cols-2 py-32 px-14"}>
+    <>
+      <div className={"grid md:grid-cols-2 pt-32"}>
         <UserBar/>
         <UserLine/>
       </div>
+      <UserTable/>
+    </>
+      
   )
 }
 
