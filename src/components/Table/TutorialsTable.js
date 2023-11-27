0a2e43f94902bc5465ade2a1f7b97805b3a2355f
Chea Chento <1220.chea.chento@rupp.edu.kh>
2023-11-27 10:01:11 +0700

    tutorial

--- src/components/Table/TutorialsTable.js
+++ src/components/Table/TutorialsTable.js
@@ -11,7 +11,12 @@ import { Pagination } from "@nextui-org/react";
 import { Select, SelectItem } from "@nextui-org/react";
 import { Input } from "@nextui-org/react";
 import { FiSearch } from "react-icons/fi";
-import { IoIosAddCircleOutline } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { CiCircleRemove } from "react-icons/ci";
+
+import moment from "moment";
+
 
 export default function TutorialsTable() {
 
@@ -21,6 +26,7 @@ export default function TutorialsTable() {
     const [isDeleteModalTutorialOpen, setDeleteModalTutorial] = useState(false);
 
     const handleOpen = (backdrop, id) => {
+        console.log("id: ", id);
         setDeleteId(id);
         setBackdrop(backdrop)
         onOpen();
@@ -65,19 +71,31 @@ export default function TutorialsTable() {
     }
     const handleDeleteTutorial = async () => {
         await deleteTutorial(deleteId);
+        toast.success("Delete Tutorial Success!");
     }
 
 
     return (
         <>
-            {/* delete modal */}
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
             <Modal backdrop={backdrop} isOpen={isOpen} onClose={onClose}>
                 <ModalContent>
                     {(onClose) => (
                         <>
-                            <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                            <ModalBody>
-
+                            <ModalHeader className="flex flex-col gap-1 text-center">Are you sure you want to delete this tutorial?</ModalHeader>
+                            <ModalBody className="flex justify-center items-center">
+                                <CiCircleRemove size={200} color="red" />
                             </ModalBody>
                             <ModalFooter>
                                 <Button color="danger" variant="light" onPress={onClose}>
@@ -87,6 +105,8 @@ export default function TutorialsTable() {
                                 >
                                     Delete
                                 </Button>
+
+
                             </ModalFooter>
                         </>
                     )}
@@ -134,10 +154,9 @@ export default function TutorialsTable() {
             <Table aria-label="Example static collection table">
                 <TableHeader>
                     <TableColumn>TITLE</TableColumn>
-                    <TableColumn>DESCRIPTION</TableColumn>
                     <TableColumn>CREATED_BY</TableColumn>
+                    <TableColumn>DESCRIPTION</TableColumn>
                     <TableColumn>CREATED_AT</TableColumn>
-                    <TableColumn>STATUS</TableColumn>
                     <TableColumn>ACTIONS</TableColumn>
 
                 </TableHeader>
@@ -147,31 +166,19 @@ export default function TutorialsTable() {
                         <TableRow key={index}>
                             <TableCell>{item.title}</TableCell>
                             <TableCell>{item.published_by.username}</TableCell>
-                            <TableCell>{item.published_by.username}</TableCell>
-                            <TableCell>{item.created_at}</TableCell>
-                            <TableCell>{item.is_deleted.toString()}</TableCell>
+                            <TableCell>{item.description}</TableCell>
                             <TableCell>
-                                <Dropdown >
-                                    <DropdownTrigger>
-                                        <Button
-                                            variant="bordered"
-                                        >
-                                            Action
-                                        </Button>
-                                    </DropdownTrigger>
-                                    <DropdownMenu aria-label="Example with disabled actions" >
-                                        <DropdownItem key="update">UPDATE</DropdownItem>
-                                        <DropdownItem
-                                            className="text-danger"
-                                            color="danger"
-                                            onPress={() => handleOpen("blur", item.id)}
-                                            key={"blur"}
-                                        >
-                                            DELETE
-                                        </DropdownItem>
-
-                                    </DropdownMenu>
-                                </Dropdown>
+                                {moment(item.created_at).format("YYYY-MM-DD")}
+                            </TableCell>
+                            <TableCell>
+                                <Button color="success" variant="bordered" key={"blur"} >
+                                    <FaRegEdit />
+                                </Button>
+                                <Button color="danger" className="ml-4" variant="bordered" onPress={() => handleOpen("blur", item.id)}
+                                    key={"blur"} >
+                                    <MdDelete />
+                                </Button>
+                               
                             </TableCell>
                         </TableRow>
                     )
@@ -188,7 +195,7 @@ export default function TutorialsTable() {
                     initialPage={page}
                     onChange={handlePageChange}
                 />
-                <Select labelPlacement="1" className="max-w-xs w-28 ml-14" onChange={handleSetPage}>
+                <Select labelPlacement="1" className="max-w-xs w-28 ml-14" onChange={handleSetPage} defaultSelectedKeys={["10"]}>
                     <SelectItem key="10" value={10} defaultSelected>
                         10
                     </SelectItem>
